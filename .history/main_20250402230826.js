7bb862037f426f1dee956a7b63014c7c3fdcccc5
Matheus Vinicius Cervelheri <mcervelheri@gmail.com>
2025-04-02 23:09:26 -0300

    processos relacionados aos gráficos, foram comentados

--- .history/main_20250402230826.js
+++ .history/main_20250402230826.js
@@ -172,6 +172,6 @@ for (const formElement of form) {
   }
 }
 
-// form.addEventListener("submit", renderProgression);
+form.addEventListener("submit", renderProgression);
 
 clanerButton.addEventListener("click", clearForm);
